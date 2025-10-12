4b57d3ffc304cf94783a78bf5f79c2a5a6086835
Jayedkhan800 <tanvirgaming859@gmail.com>
2025-10-12 13:36:27 +0600

    blossom: Add libshim_vtservice for AudioTrack and Surface/MediaMuxer symbols
    libshim_vtservice to handle missing legacy symbols required
    
    Signed-off-by: Jayedkhan800 <tanvirgaming859@gmail.com>

--- libshims/libshim_vtservice.cpp
+++ libshims/libshim_vtservice.cpp
@@ -9,6 +9,7 @@ using namespace android;
 
 extern "C" {
 
+// Real AudioTrack constructor symbol with IAudioTrackCallback
 void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tj20audio_output_flags_tRKNS_2wpINS0_19IAudioTrackCallbackEEEi15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfi(
     void *thisptr, audio_stream_type_t streamType, uint32_t sampleRate,
     audio_format_t format, audio_channel_mask_t channelMask, size_t frameCount,
@@ -21,23 +22,28 @@ void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_cha
     const audio_attributes_t *pAttributes, bool doNotReconnect,
     float maxRequiredSpeed, audio_port_handle_t selectedDeviceId);
 
+// Shim constructor that takes legacy callback
 void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tjiPK18audio_attributes_tbfi(
     void *thisptr, audio_stream_type_t streamType, uint32_t sampleRate,
     audio_format_t format, audio_channel_mask_t channelMask, size_t frameCount,
-    audio_output_flags_t flags, android::AudioTrack::legacy_callback_t cbf,
+    audio_output_flags_t flags,
+    void (*cbf)(int event, void* user, void* info),
     void *user, int32_t notificationFrames, audio_session_t sessionId,
     android::AudioTrack::transfer_type transferType,
     const audio_offload_info_t *offloadInfo, uid_t uid, pid_t pid,
     const audio_attributes_t *pAttributes, bool doNotReconnect,
     float maxRequiredSpeed, audio_port_handle_t selectedDeviceId) {
+  
   AttributionSourceState attributionSource;
   attributionSource.uid = uid;
   attributionSource.pid = pid;
   attributionSource.packageName = "com.mediatek.ims";
   attributionSource.token = android::sp<android::BBinder>::make();
   attributionSource.attributionTag = std::nullopt;
-  android::wp<LegacyCallbackWrapper> cbfWrapper =
+
+  android::wp<android::AudioTrack::IAudioTrackCallback> cbfWrapper =
       new LegacyCallbackWrapper(cbf, user);
+
   _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tj20audio_output_flags_tRKNS_2wpINS0_19IAudioTrackCallbackEEEi15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfi(
       thisptr, streamType, sampleRate, format, channelMask, frameCount, flags,
       cbfWrapper, notificationFrames, sessionId, transferType, offloadInfo,
@@ -45,21 +51,26 @@ void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_o
       selectedDeviceId);
 }
 
+// Surface shim
 void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(
     void *thisptr, const sp<IGraphicBufferProducer> &bufferProducer,
     bool controlledByApp, const sp<IBinder> &surfaceControlHandle);
 
 void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb(
-        void *thisptr, const sp<IGraphicBufferProducer> &bufferProducer,
+    void *thisptr, const sp<IGraphicBufferProducer> &bufferProducer,
     bool controlledByApp) {
   _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(
       thisptr, bufferProducer, controlledByApp, nullptr);
 }
 
-void _ZN7android10MediaMuxerC1EiNS_14MediaMuxerBase12OutputFormatE(void *thisptr, int fd, android::MediaMuxer::OutputFormat format);
+// MediaMuxer shim
+void _ZN7android10MediaMuxerC1EiNS_14MediaMuxerBase12OutputFormatE(
+    void *thisptr, int fd, android::MediaMuxer::OutputFormat format);
 
 void _ZN7android10MediaMuxerC1EiNS0_12OutputFormatE(
     void *thisptr, int fd, android::MediaMuxer::OutputFormat format) {
-  _ZN7android10MediaMuxerC1EiNS_14MediaMuxerBase12OutputFormatE(thisptr, fd,format);
+  _ZN7android10MediaMuxerC1EiNS_14MediaMuxerBase12OutputFormatE(
+      thisptr, fd, format);
 }
-}
+
+} // extern
