5d3a51d8fe8ac1bd2c99aeb5d755e57ff1415ee3
Jayedkhan800 <tanvirgaming859@gmail.com>
2025-10-15 00:32:26 +0600

    blossom : linkup apply_camera

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@ deviceDir=$(gettop)/device/xiaomi/blossom/
 
 # apply patches
 ${deviceDir}/applyPatches.sh ${deviceDir}/patches
-
+${deviceDir}/apply_camera.sh
 # For now, just skip the ABI checks to fix build errors.
 export SKIP_ABI_CHECKS=true
 
